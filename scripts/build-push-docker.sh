9f48eb0324e636dba04839f1281ebff7826abfdf
Klaus Wening <klaus.wening@datev.de>
2022-12-28 19:45:15 +0100

    Update build-push-docker script

--- scripts/build-push-docker.sh
+++ scripts/build-push-docker.sh
@@ -2,7 +2,11 @@
 IMAGE_NAME="kwening/artifactory-config"
 TAG=$(date +"%Y%m%d-%H%M%S")
 
+echo "INFO: Re-creating requirements.txt"
+poetry export -f requirements.txt --output requirements.txt
+
+echo "INFO: Building image '$IMAGE_NAME:$TAG'"
 buildah bud -t docker.io/$IMAGE_NAME:$TAG --format docker .
 buildah push docker.io/$IMAGE_NAME:$TAG
 
-echo "Image docker.io/$IMAGE_NAME:$TAG successfully built"
+echo "INFO: Image 'docker.io/$IMAGE_NAME:$TAG' successfully built"
